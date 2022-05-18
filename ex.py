bc5e8bf4d7ea8723c7b3c4194d847b6665891ae4
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-05-18 13:22:39 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,54 +1,3 @@
-# Problema: Contratação
-# Utilizando orientação a objetos criar uma aplicação que insira uma pessoa em uma
-# lista e valide os dados da pessoa.
-
-# A estrutura que representará a pessoa deve possuir os seguintes atributos:
-# Nome
-# Sobrenome
-# CPF
-# Data de Nascimento
-
-# Todas as informações são obrigatórias.
-
-
-# Caso alguma informação não seja preenchida deve-se apresentar uma mensagem
-# informando que a informação específica é obrigatória e que a pessoa não foi adicionada à lista.
-# Ex: de mensagem: "A data de nascimento é obrigatória. O Cliente não foi adicionado à lista".
-
-
-# Não deve ser possível adicionar pessoas com o mesmo CPF. Validar quantidade total de candidatos por vaga.
-
-
-# Associar candidato por vaga. A listagem de pessoas deve conter as informações:
-
-
-# Nome
-# Sobrenome
-# CPF
-# Data de nascimento
-# É maior de idade?
-# Vaga
-
-
-# A empresa XPTO precisa fazer um sistema de cadastro de candidatos para seu programa de Trainee.
-# O cadastro será feito em 2 momentos, mas na mesma tela.
-
-# 1º será para adicionar as informações do candidato.
-
-# 2º agrupar os candidatos preenchidos no passo anterior em uma tabela/grid para ao clicar no botão de SALVAR enviar para gravar
-# todos de uma única vez.
-
-# Inicialmente o usuário precisará fazer o cadastro de acordo com o nº de vagas disponíveis na empresa, o primeiro processo
-# será de 5 vagas, a quantidade máxima de candidatos por vaga será 3, caso o usuário tente cadastrar mais candidatos do que
-# vagas o sistema precisa validar e avisar que o nº de candidatos já atingiu o limite permitido.
-
-# As informações necessárias para inserir um candidato é Nome, Sobrenome, CPF e Data de nascimento e Vaga, todas as
-# informações são obrigatórias, caso não for preenchido o usuário deve ser avisado sobre qual campo está com problema
-# e o mesmo candidato não pode ser inserido 2 vezes.
-
-# A tabela que irá agrupar os candidatos vai conter as informações Nome, Sobrenome, CPF, Data de Nascimento, Idade,
-# É maior de idade? e Vaga (ao qual o candidato foi associado). E por fim um botão para confirmar o cadastro de todos os
-# candidatos e avisar ao usuário que os candidatos foram cadastrados com sucesso.
 import datetime
 
 print("Sistema de Cadastro de Candidatos")
@@ -66,14 +15,20 @@ data = datetime.datetime.now()
 data_atual = data.date()
 
 contador = 0
+
 quantidade_vagas = 5
 max_vagas = quantidade_vagas * 3
-candidatos = int(input(
-    f"Quantos candidatos deseja cadastrar (Máximo de {max_vagas} candidatos): "))
+
+candidatos = input(
+    f"Quantos candidatos deseja cadastrar (Máximo de {max_vagas} candidatos): ")
+while candidatos == "":
+    candidatos = input(
+        f"\033[1;31mErro! Informe novamente quantos candidatos deseja cadastrar (Máximo de {max_vagas} candidatos): \033[m")
+candidatos = int(candidatos)
 
 if candidatos > max_vagas:
     print(
-        f"Erro! Não pode cadastrar mais que {max_vagas} candidatos nesse momento")
+        f"\033[1;31mErro! Não pode cadastrar mais que {max_vagas} candidatos nesse momento\033[m")
     candidatos = int(input(
         f"Quantos candidatos deseja cadastrar (Máximo de {max_vagas} candidatos): "))
 
@@ -85,7 +40,7 @@ while contador < candidatos:
         f"Informe o nome do {contador + 1}° candidato: ")
     while nome == "":
         nome = input(
-            f"Erro! Informe novamente o nome do {contador + 1}° candidato: ")
+            f"\033[1;31mErro! Informe novamente o nome do {contador + 1}° candidato: \033[m")
     lista_nomes.append(nome)
 
     # Sobrenome
@@ -93,7 +48,7 @@ while contador < candidatos:
         f"Informe o sobrenome do candidato: ")
     while sobrenome == "":
         sobrenome = input(
-            f"Erro! Informe novamente o sobrenome do candidato: ")
+            f"\033[1;31mErro! Informe novamente o sobrenome do candidato: \033[m")
     lista_sobrenomes.append(sobrenome)
 
     # CPF
@@ -101,21 +56,36 @@ while contador < candidatos:
         f"Informe o CPF do candidato: ")
     while cpf == "":
         cpf = input(
-            f"Erro! Informe novamente o CPF do candidato: ")
+            f"\033[1;31mErro! Informe novamente o CPF do candidato: \033[m")
     cpf = float(cpf)
     for i in lista_cpfs:
         if i == cpf:
             cpf = input(
-                f"Erro! Esse CPF já existe em nossos dados, informe novamente o CPF do candidato: ")
+                f"\033[1;31mErro! Esse CPF já existe em nossos dados, informe novamente o CPF do candidato: \033[m")
     lista_cpfs.append(cpf)
 
     # Data de Nascimento
-    dia = int(input(
-        f"Informe o dia em que o candidato: "))
-    mes = int(input(
-        f"Informe o mês em que o candidato: "))
-    ano = int(input(
-        f"Informe o ano em que o candidato: "))
+    dia = input(
+        f"Informe o dia em que o candidato nasceu: ")
+    while dia == "":
+        dia = input(
+            f"\033[1;31mErro! Informe novamente o dia em que o candidato nasceu: \033[m")
+    dia = int(dia)
+
+    mes = input(
+        f"Informe o mês em que o candidato nasceu: ")
+    while mes == "":
+        mes = input(
+            f"\033[1;31mErro! Informe novamente o mês em que o candidato nasceu: \033[m")
+    mes = int(mes)
+
+    ano = input(
+        f"Informe o ano em que o candidato nasceu: ")
+    while ano == "":
+        ano = input(
+            f"\033[1;31mErro! Informe novamente o ano em que o candidato nasceu: \033[m")
+    ano = int(ano)
+
     dataNasc = datetime.date(ano, mes, dia)
     lista_datanasc.append(dataNasc)
 
@@ -123,30 +93,43 @@ while contador < candidatos:
     diferenca_datas = (data_atual - dataNasc)
     idade = diferenca_datas.days / 365
     if idade >= 18:
-        lista_maior_idade.append("Maior de Idade")
+        lista_maior_idade.append("Sim")
     else:
-        lista_maior_idade.append("Menor de Idade")
+        lista_maior_idade.append("Não")
 
     # Vaga
     vaga = input(
         f"Informe a vaga que o candidato foi associado: ")
     while vaga == "":
         vaga = input(
-            f"Erro! Informe novamente a vaga que o candidato foi associado: ")
+            f"\033[1;31mErro! Informe novamente a vaga que o candidato foi associado: \033[m")
     lista_vaga.append(vaga)
 
-    print("Candidato Cadastrado com Sucesso!")
+    print("\033[1;32mCandidato Cadastrado com Sucesso!\033[m")
 
     contador += 1
 
-print("Todos os Candidatos foram cadastros!")
+print("=====================================================================")
+print("\033[1;32mTodos os Candidatos foram cadastros!\033[m")
 
-revisar = input("Gostaria de revisar todos os candidatos cadastrados? [S/N] ")
+revisar = input("Gostaria de revisar todos os candidatos cadastrados? [S/N]: ")
 while revisar == "":
     revisar = input(
-        f"Erro! Gostaria de revisar todos os candidatos cadastrados? [S/N] ")
+        f"\033[1;31mErro! Gostaria de revisar todos os candidatos cadastrados? [S/N]: \033[m")
 
 if revisar[0].capitalize() == "S":
-    print("Oi")
+    contador = 0
+    print("")
+    while contador < candidatos:
+        print("========================================")
+        print(f"Nome: {lista_nomes[contador]}")
+        print(f"Sobrenome: {lista_sobrenomes[contador]}")
+        print(f"CPF: {lista_cpfs[contador]}")
+        print(f"Data de Nascimento: {lista_datanasc[contador]}")
+        print(f"Maior de Idade: {lista_maior_idade[contador]}")
+        print(f"Vaga: {lista_vaga[contador]}")
+        print("========================================")
+        contador += 1
+
 if revisar[0].capitalize() == "N":
     print("Finalizando sistema de cadastro de candidatos!")
